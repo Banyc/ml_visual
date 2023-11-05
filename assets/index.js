a499e4dda372f1006d44fe24df6e2e1d8568f418
Banyc <36535895+Banyc@users.noreply.github.com>
2023-11-05 15:45:13 +0800

    feat: js reads string from wasm memory

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,35 @@
-let lib_wasm_filename = "lib.wasm"
+const lib_wasm_filename = "lib.wasm"
 let lib_wasm = null
+let lib_wasm_buffer = null
 
 async function import_lib_wasm() {
-    let results = await WebAssembly.instantiateStreaming(fetch(lib_wasm_filename))
+    const results = await WebAssembly.instantiateStreaming(fetch(lib_wasm_filename))
     lib_wasm = results.instance.exports
+    lib_wasm_buffer = results.instance.exports.memory.buffer
     console.log("Imported `%s`", lib_wasm_filename)
+}
+
+function read_wasm_buffer(buffer_offset, length) {
+    const buffer_ptr = lib_wasm.buffer()
+    const buffer = new Uint8Array(lib_wasm_buffer).slice(buffer_ptr)
+    return buffer.slice(buffer_offset, length)
+}
+
+function read_wasm_string(buffer_offset, length) {
+    const buffer = read_wasm_buffer(buffer_offset, length)
+    const string = new TextDecoder().decode(buffer);
+    return string
 }
 
 async function main() {
     await import_lib_wasm()
 
     let span_sum = document.getElementById("sum")
-    let sum = lib_wasm.add(1, 1)
+    const sum = lib_wasm.add(1, 1)
     span_sum.textContent = sum
+
+    const len = lib_wasm.buffer_write_hello_world()
+    console.log("`%s` said: \"%s\"", lib_wasm_filename, read_wasm_string(0, len))
 }
 
 main()
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,22 @@
+use std::io::Write;
+
+const WASM_MEMORY_BUFFER_SIZE: usize = 1024;
+pub static mut WASM_MEMORY_BUFFER: [u8; WASM_MEMORY_BUFFER_SIZE] = [0; WASM_MEMORY_BUFFER_SIZE];
+
+#[no_mangle]
+pub extern "C" fn buffer() -> *const u8 {
+    unsafe { WASM_MEMORY_BUFFER.as_ptr() }
+}
+
 #[no_mangle]
 pub extern "C" fn add(a: isize, b: isize) -> isize {
     a + b
+}
+
+#[no_mangle]
+pub extern "C" fn buffer_write_hello_world() -> usize {
+    let mut buffer = unsafe { WASM_MEMORY_BUFFER.as_mut() };
+    let str = b"hello world";
+    buffer.write_all(str).unwrap();
+    str.len()
 }
