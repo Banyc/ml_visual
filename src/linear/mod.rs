dea2e59f416749e5f7f1de58c2512142ae4a40a5
Banyc <36535895+Banyc@users.noreply.github.com>
2023-12-07 13:08:12 +0800

    refactor: bump crates

--- src/linear/mod.rs
+++ src/linear/mod.rs
@@ -1,4 +1,4 @@
-use math::statistics::StandardizedExt;
+use math::transformer::{standard_scaler::StandardScaler, TransformExt};
 
 use self::models::{MulticlassExample, TwoFeatures};
 
@@ -101,11 +101,13 @@ pub fn standardize(
     let x_1 = examples
         .clone()
         .map(|example| example.feature().x_1())
-        .standardized();
+        .fit_transform::<StandardScaler>()
+        .unwrap();
     let x_2 = examples
         .clone()
         .map(|example| example.feature().x_2())
-        .standardized();
+        .fit_transform::<StandardScaler>()
+        .unwrap();
 
     x_1.zip(x_2)
         .map(|(x_1, x_2)| TwoFeatures::new(x_1, x_2))
